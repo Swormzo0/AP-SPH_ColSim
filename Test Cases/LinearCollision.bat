1c5674844374eb6935c0dc71aa12d4da6daa88b9
Swormzo0 <cryptic.anonymity@gmail.com>
2015-10-13 07:03:10 -0400

    data-1D-1
    Data for 1D collision in Excel

---
+++ Test Cases/LinearCollision.bat
@@ -0,0 +1,8 @@
+@echo off
+set OPTIONS=2 6 0 12 0 0 0
+rem set OPTIONS=1 2 180 -.2 0 1 0
+rem set OPTIONS=2 1 0 1 0 -.2 0
+rem set OPTIONS=8 5 307.75 4.5 0 0 0
+echo 0 %OPTIONS% | AP_SPH_CollisionSim > LinearA.xls
+echo 1 %OPTIONS% | AP_SPH_CollisionSim > LinearB.xls
+
